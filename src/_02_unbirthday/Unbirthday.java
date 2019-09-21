7b1e71461326513afe28c1e3da7c65ecae1c54f9
League Student <league@iMac-10.attlocal.net>
2019-09-21 11:29:46 -0700

    module 1

---
+++ src/_02_unbirthday/Unbirthday.java
@@ -0,0 +1,10 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		String birthday=JOptionPane.showInputDialog("what is your birthday, month, day, year");
+		JOptionPane.showMessageDialog(null,"your birthday is "+birthday);
+		}
+	}
